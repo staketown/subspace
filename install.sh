0e008d793c0e665870dd00df09d8fae81a410187
mogoll92 <mogoll92@gmail.com>
2023-03-22 08:46:50 +0200

    subspace script

--- install.sh
+++ install.sh
@@ -56,8 +56,8 @@ function updateNode {
     cd $HOME || return
     wget -O subspace-cli https://github.com/subspace/subspace-cli/releases/download/v0.1.10-alpha/subspace-cli-Ubuntu-x86_64-v3-v0.1.10-alpha
     sudo chmod +x subspace-cli
+    sudo rm /usr/local/bin/subspace-cli
     sudo mv subspace-cli /usr/local/bin/
-    sudo rm -rf $HOME/.config/subspace-cli
 
     source $HOME/.bash_profile
     sudo systemctl restart subspaced
