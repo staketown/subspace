e9421b9b3a989112d8ccfee7704fd1dd83910aac
mogoll92 <mogoll92@gmail.com>
2023-03-30 20:33:08 +0300

    Updated script

--- install.sh
+++ install.sh
@@ -4,7 +4,23 @@ source <(curl -s https://raw.githubusercontent.com/R1M-NODES/utils/master/common
 
 printLogo
 
-URL=https://github.com/subspace/subspace-cli/releases/download/v0.1.12/subspace-cli-Ubuntu-x86_64-v3-v0.1.12
+VERSION=$(echo 'BEGIN {
+    while (!/flags/) if (getline < "/proc/cpuinfo" != 1) exit 1
+    if (/lm/&&/cmov/&&/cx8/&&/fpu/&&/fxsr/&&/mmx/&&/syscall/&&/sse2/) level = 1
+    if (level == 1 && /cx16/&&/lahf/&&/popcnt/&&/sse4_1/&&/sse4_2/&&/ssse3/) level = 2
+    if (level == 2 && /avx/&&/avx2/&&/bmi1/&&/bmi2/&&/f16c/&&/fma/&&/abm/&&/movbe/&&/xsave/) level = 3
+    if (level == 3 && /avx512f/&&/avx512bw/&&/avx512cd/&&/avx512dq/&&/avx512vl/) level = 4
+    if (level > 0) { print level; exit level + 1 }
+    exit 1
+}' | awk -f -)
+
+if [[ $VERSION -ne 2 && $VERSION -ne 3 ]]
+  then
+    echo  "Your processor doesn't support subspace"
+    exit
+fi
+
+URL=https://github.com/subspace/subspace-cli/releases/download/v0.1.12/subspace-cli-Ubuntu-x86_64-v${VERSION}-v0.1.12
 
 PS3='Enter your option: '
 options=("Install node" "Update node")
